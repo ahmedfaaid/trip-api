6c22c0c61120c6e6984a785e83a1e0fc070230d1
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-03-31 17:38:12 -0400

    remove session store

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import session from 'express-session';
-import sqliteStoreFactory from 'express-session-sqlite';
-import * as sqlite3 from 'sqlite3';
 import { AppModule } from './app.module';
-
-const SqliteStore = sqliteStoreFactory(session);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(
     session({
-      store: new SqliteStore({
-        driver: sqlite3.Database,
-        path: 'db.sqlite',
-        ttl: 5 * 24 * 60 * 60 * 1000 // 5 days
-      }),
       name: 'tripfare-qid',
       secret: process.env.SESSION_SECRET as string,
       resave: false,
