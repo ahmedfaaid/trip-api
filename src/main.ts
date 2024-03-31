1130879b397a5187001adefc7636b5d28334d99f
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-03-31 14:24:17 -0400

    express session

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
+import session from 'express-session';
+import sqliteStoreFactory from 'express-session-sqlite';
+import * as sqlite3 from 'sqlite3';
 import { AppModule } from './app.module';
+
+const SqliteStore = sqliteStoreFactory(session);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      store: new SqliteStore({
+        driver: sqlite3.Database,
+        path: 'db.sqlite',
+        ttl: 5 * 24 * 60 * 60 * 1000 // 5 days
+      }),
+      name: 'tripfare-qid',
+      secret: process.env.SESSION_SECRET as string,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 5 * 24 * 60 * 60 * 1000 // 5 days
+      }
+    })
+  );
   await app.listen(5100);
 }
 bootstrap();
