079819a40ce2e98daae46c4e26170b28ad61e336
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-04-08 20:54:39 -0400

    register controller & service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,15 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Req,
+  UploadedFile,
+  UseInterceptors
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { Request } from 'express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 
@@ -10,5 +20,28 @@ export class AuthController {
   @Post('login')
   async login(@Body() loginDto: LoginDto, @Req() req: Request) {
     return await this.authService.login(loginDto, req);
+  }
+
+  @Post('register')
+  @UseInterceptors(
+    FileInterceptor('profile_picture', {
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, callback) => {
+          const userData = JSON.parse(req.body.data);
+          const ext = extname(file.originalname);
+          const filename = `${userData.username}-${Date.now()}${ext}`;
+          callback(null, filename);
+        }
+      })
+    })
+  )
+  async register(
+    @Body() body: any,
+    @Req() req: Request,
+    @UploadedFile() profile_picture?: Express.Multer.File
+  ) {
+    const data = JSON.parse(body.data);
+    return await this.authService.register(data, req, profile_picture);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,13 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 import { UserService } from 'src/user/user.service';
+import { omit } from 'utils/fns';
 import { LoginDto } from './dto/login.dto';
 
 @Injectable()
@@ -33,6 +39,34 @@ export class AuthService {
     } catch (error) {
       console.error({ error });
       throw new UnauthorizedException();
+    }
+  }
+
+  async register(
+    body: CreateUserDto,
+    req: Request,
+    profile_picture?: Express.Multer.File
+  ) {
+    console.log({ body });
+    try {
+      let newUser = await this.userService.create(body, profile_picture);
+
+      if (!newUser) throw new BadRequestException();
+
+      newUser = omit(['password'], newUser);
+
+      const sessionPayload = {
+        username: newUser.username,
+        id: newUser.id
+      };
+
+      req.session.username = newUser.username;
+      req.session.token = await this.jwtService.signAsync(sessionPayload);
+
+      return newUser;
+    } catch (error) {
+      console.error({ error });
+      throw new BadRequestException();
     }
   }
 }
