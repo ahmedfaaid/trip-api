717b7e076754e265383ea3788aa2454e52a20bf1
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-04-08 20:48:11 -0400

    session username type

--- types/express-session.d.ts
+++ types/express-session.d.ts
@@ -2,7 +2,7 @@ import 'express-session';
 
 declare module 'express-session' {
   export interface SessionData {
-    username: { [key: string]: string };
+    username: string;
     token: string;
   }
 }
